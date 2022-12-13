6c3b0121f9f7764d162325483bddc28557af26ef
RomanZollo <zloyandmad@yandex.ru>
2022-12-13 22:31:16 +0200

    top-slider added

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,32 @@
 import $ from "jquery";
 
-import * as flsFunctions from "./modules/functions.js";
+import Swiper from 'swiper/bundle';
 
+
+import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
+const swiper = new Swiper('.top-slider__swiper', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
 $(function () {
-  $('.menu__btn').on('click', function() {
+  $('.menu__btn').on('click', function () {
     $('.menu__btn').toggleClass('menu__btn--active');
     $('.menu__list').toggleClass('menu__list--active');
   });
