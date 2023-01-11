3606797ec9d1a6e08e48e330a650ca23c67c15af
RomanZollo <zloyandmad@yandex.ru>
2023-01-11 17:10:07 +0200

    about-num and team added

--- src/js/app.js
+++ src/js/app.js
@@ -37,8 +37,13 @@ var config = {
   }
 };
 
-var mixer1 = mixitup(containerEl1, config);
-var mixer2 = mixitup(containerEl2, config);
+if (containerEl1) {
+  var mixer1 = mixitup(containerEl1, config);
+}
+
+if (containerEl2) {
+  var mixer2 = mixitup(containerEl2, config);
+}
 
 
 
@@ -55,3 +60,32 @@ $(function () {
     $(this).toggleClass('title-show--active');
   });
 })
+
+// счетчик цифр
+
+
+let $element = $('.about-num__number');
+let counter = 0;
+
+$(window).on('scroll', (function () {
+  var scroll = $(window).scrollTop() + $(window).height();
+  //Если скролл до конца елемента
+  var offset = $element.offset().top + $element.height();
+  //Если скролл до начала елемента
+  // var offset = $element.offset().top
+
+  if (scroll > offset && counter == 0) {
+    $('.about-num__number').each(function () {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 1500,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+    counter = 1;
+  }
+}));
